6df963cc3ef7bcc490c34d9c9d872a7ee41864bb
EMEA\leerya <Ryan.Lee@externals.adidas.com>
2022-08-10 10:08:11 +0700

    feat: add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true, // IMPORTANT!!! REMOVE WHEN STABLE
   },
-  productionBrowserSourceMaps: true,
+  productionBrowserSourceMaps: true, // IMPORTANT!!! REMOVE IN PRODUCTION RELEASE
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
